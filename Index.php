e7a9142a01dc382c5210b9d5f80800fc95eea145
GalinIGeorgiev <galin.georgiev.ggg@gmail.com>
2024-10-24 22:04:07 +0300

    Add css and Index changes

--- Index.php
+++ Index.php
@@ -2,16 +2,23 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/reset.css">
     <title>Document</title>
 </head>
 <body>
-    <?php
-        echo "hello" ;
-    ?>
+    
+    <h4>Signup</h4>
 
-    <form action=""></form>
-    <form action=""></form>
-    <form action=""></form>
+    <form action="">
+        <input type="text" name="username" placeholder="Username">
+        <input type="password" name="pwd" placeholder="Password">
+        <input type="text" name="email" placeholder="E-mail">
+        <button>Signup</button>
+    </form>
+
+   
 </body>
 </html>
