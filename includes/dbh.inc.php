41a41e1abed3d36de607452f4645a00b027328b3
GalinIGeorgiev <galin.georgiev.ggg@gmail.com>
2024-10-26 23:28:25 +0300

    Db connection Changes

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,15 +1,27 @@
 <?php
 
-$dsn = "mysql:host=localhost;dbname=myfirstdatabase" ;
 $dbusername = "root";
-$dbpassword = "root";
+$dbpassword = "";
+$dbname = "mydatabase";
+
+$dsn = "mysql:host=localhost;dbname=mydatabase";
+$dbusername = "root";
+$dbpassword = "";
+
+
+
+$dsn = "mysql:host=localhost;dbname=mydatabase;charset=utf8";
+$opt = array(
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+);
+$pdo = new PDO($dsn,$dbusername,$dbpassword, $opt);
 
 
 try {
-    $pdo = new PDO($dsn, $dbpassword, $dbusername)
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ATTR_ERRMODE_EXCEPTION);
-    $pdo->prepare
-
+    $pdo = new PDO($dsn,$dbusername,$dbpassword, $opt);
+    
+    
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage;
     //throw $th;
